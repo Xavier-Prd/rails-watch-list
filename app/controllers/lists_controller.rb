97c48caa4c2bd67ca60610a8445e2b0f118500ea
Xavier Pardoue <pardoue.xavier@gmail.com>
2026-02-27 15:58:46 +0100

    fix list_create

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -17,8 +17,11 @@ class ListsController < ApplicationController
 
   def create
     @list = List.new(list_params)
-    @list.save
-    redirect_to list_path(@list)
+    if @list.save
+      redirect_to @list
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
